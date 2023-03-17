9e0cdec69eff9f4bd6472023129430f5cfe0a396
Callum Stew <callum.d.stew@gmail.com>
2023-03-17 22:39:19 +0000

    4

--- ddot.c
+++ ddot.c
@@ -11,16 +11,22 @@
  */
 int ddot (const int n, const double * const x, const double * const y, double * const result) {  
   double local_result = 0.0;
-  if (y==x){
-    for (int i=0; i<n; i++) {
-      local_result += x[i]*x[i];
-    }
-  } else {
-    for (int i=0; i<n; i++) {
-      local_result += x[i]*y[i];
+  #pragma omp parallel reduction(+:local_result)
+  {
+    if (y==x){
+      #pragma omp for nowait
+      for (int i=0; i<n; i++) {
+        local_result += x[i]*x[i];
+      }
+
+    } else {
+      #pragma omp for nowait
+      for (int i=0; i<n; i++) {
+        local_result += x[i]*y[i];
+      }
     }
   }
-  *result = local_result;
 
+  *result = local_result;
   return 0;
 }
