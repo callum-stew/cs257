af7b760e67ad4f7d97c97306b71979fe2f897278
Callum Stew <callum.d.stew@gmail.com>
2023-03-18 11:55:25 +0000

    l

--- sparsemv.c
+++ sparsemv.c
@@ -15,6 +15,7 @@
  */
 int sparsemv(struct mesh *A, const double * const x, double * const y)
 {
+  const int loopFactor = 8;
 
   const int nrow = (const int) A->local_nrow;
 
@@ -24,11 +25,26 @@ int sparsemv(struct mesh *A, const double * const x, double * const y)
       const double * const cur_vals = (const double * const) A->ptr_to_vals_in_row[i];
       const int * const cur_inds = (const int * const) A->ptr_to_inds_in_row[i];
       const int cur_nnz = (const int) A->nnz_in_row[i];
+      const int N = (cur_nnz/loopFactor)*loopFactor;
+      int j;
 
-      for (int j=0; j< cur_nnz; j++) {
-        sum += cur_vals[j]*x[cur_inds[j]];
+
+      for (j=0; j<N; j+=loopFactor) {
+        sum += (cur_vals[j]*x[cur_inds[j]]) \
+             + (cur_vals[j+1]*x[cur_inds[j+1]]) \
+             + (cur_vals[j+2]*x[cur_inds[j+2]]) \
+             + (cur_vals[j+3]*x[cur_inds[j+3]]) \
+             + (cur_vals[j+4]*x[cur_inds[j+4]]) \
+             + (cur_vals[j+5]*x[cur_inds[j+5]]) \
+             + (cur_vals[j+6]*x[cur_inds[j+6]]) \
+             + (cur_vals[j+7]*x[cur_inds[j+7]]);
+      }
+
+      for (; j<cur_nnz; j++) {
+        sum += (cur_vals[j]*x[cur_inds[j]]);
       }
       y[i] = sum;
-    }
+  }
+
   return 0;
 }
