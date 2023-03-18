3787a7390e10848a780c8f029041fdd443ff3363
Callum Stew <callum.stew@warwick.ac.uk>
2023-03-18 17:14:11 +0000

    change-6

--- ddot.c
+++ ddot.c
@@ -1,3 +1,5 @@
+#include <immintrin.h>
+
 #include "ddot.h"
 
 /**
@@ -11,16 +13,35 @@
  */
 int ddot (const int n, const double * const x, const double * const y, double * const result) {  
   double local_result = 0.0;
-  if (y==x){
-    for (int i=0; i<n; i++) {
-      local_result += x[i]*x[i];
+  __m256d v_r = _mm256_set1_pd(0.0);
+  __m256d v_r_total = _mm256_set1_pd(0.0);
+
+  const int loopFactor = 4;
+  const int loopN = (n/loopFactor)*loopFactor;
+
+  #pragma omp parallel firstprivate(v_r) shared(v_r_total)
+  {
+    #pragma omp for
+    for (int i=0; i<loopN; i+=loopFactor) {
+      __m256d v_x = _mm256_load_pd(x+i);
+      __m256d v_y = _mm256_load_pd(y+i);
+      v_r = _mm256_add_pd(_mm256_mul_pd(v_x, v_y), v_r);
     }
-  } else {
-    for (int i=0; i<n; i++) {
-      local_result += x[i]*y[i];
+    #pragma omp critical
+    {
+      v_r_total = _mm256_add_pd(v_r, v_r_total);
     }
   }
-  *result = local_result;
 
+  for (int i=loopN; i<n; i++) {
+    local_result += x[i]*y[i];
+  }
+
+  double * r_array = _mm_malloc(sizeof(double)*4, 32);
+  _mm256_store_pd(r_array, v_r_total);
+  local_result += r_array[0] + r_array[1] + r_array[2] + r_array[3];
+  _mm_free(r_array);
+
+  *result = local_result;
   return 0;
 }
